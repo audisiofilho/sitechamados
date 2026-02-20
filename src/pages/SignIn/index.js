8dd2f48a77d22eccc21d882c7a1d99ebd297375d
audisiofilho <obuzua46@gmail.com>
2026-02-20 11:42:51 -0300

    Criando e estilizando pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import './signin.css';
+
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
-    <div>
-      <h1>Sign In</h1>
-      <p>This is the Sign In page.</p>
+    <div className='container-center'>
+      <div className='login'>
+        <div className='login-area'>
+          <img src={logo} alt='Logo do sistema' />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input 
+          type='text' 
+          placeholder='email@email.com' 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}/>
+          <input 
+          type='password' 
+          placeholder='********' 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}/>
+
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link to='/signup'>Criar uma conta</Link>
+
+      </div>
     </div>
   );
 }
