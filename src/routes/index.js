c637916405c44e0d98c38effd679f24d1e812369
audisiofilho <obuzua46@gmail.com>
2026-02-20 11:53:03 -0300

    Criando pagina de cadastro e reutilizando estilos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ import { Route, Routes} from 'react-router-dom'
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Dashboard from '../pages/Dashboard';
+
 function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
