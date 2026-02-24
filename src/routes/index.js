85a9c4ce310874b7ff1ffee601c7971e7c787f58
audisiofilho <obuzua46@gmail.com>
2026-02-24 18:06:57 -0300

    Criando componente Title

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 import Private from './Private';
 
@@ -12,7 +13,9 @@ function RoutesApp() {
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      
       <Route path="/dashboard" element={<Private><Dashboard /></Private>} />
+      <Route path="/profile" element={<Private><Profile /></Private>} />
     </Routes>
   );
 }
