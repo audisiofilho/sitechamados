081f65fa0b666bb703b870e7ad8ca4f990c9d742
audisiofilho <obuzua46@gmail.com>
2026-02-24 21:14:59 -0300

    Criando pagina de editar perfil e estilizando

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,17 @@
+import { useContext, useState } from 'react'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 
-import { FiSettings} from 'react-icons/fi'
+import { FiSettings, FiUpload} from 'react-icons/fi'
+import avatarImg from '../../assets/avatar.png'
+import { AuthContext } from '../../contexts/auth'
+
+import './profile.css'
 
 export default function Profile() {
+    const { user } = useContext(AuthContext);
+
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
     return (
         <div>
             <Header />
@@ -11,8 +19,35 @@ export default function Profile() {
                 <Title name='Minha conta'>
                         <FiSettings size={25} />
                 </Title>
+
+                <div className='container'>
+                    <form className='form-profile'>
+                        <label className='label-avatar'>
+                            <span>
+                                <FiUpload color='#fff' size={25}/>
+                            </span>
+                            <input type='file' accept='image/*'/><br/>
+                            {avatarUrl === null ? (
+                                <img src={avatarImg} alt='Avatar do usuário' width={250} height={250}/>
+                            ) :(
+                                <img src={avatarUrl} alt='Avatar do usuário' width={250} height={250}/>
+                            )
+                            }
+                        </label>
+
+                        <label>Nome</label>
+                        <input type='text' value={user && user.name} />
+
+                        <label>Email</label>
+                        <input type='text' value={user && user.email} disabled={true} />
+
+                        <button type='submit'>Salvar</button>
+                    </form>
+                </div>
+                <div className='container'>
+                    <button className='logout-btn' type='submit'>Sair</button>
+                </div>
             </div>
-            <h1>Profile</h1>
         </div>
     )
 }
